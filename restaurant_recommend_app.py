12d1571cec2e0e3bc0a8dd8bda1f8b3a1c85c09a
bhavana sanghi <61513642+bhavanasanghi9@users.noreply.github.com>
2024-04-15 07:51:35 +0530

    Add files via upload

---
+++ restaurant_recommend_app.py
@@ -0,0 +1,113 @@
+import pickle
+import streamlit as st
+import pandas as pd 
+import numpy as np
+import re
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+st.title("Let's help you decide where to eat today!")
+
+#Get user to select the desired location
+
+location_list=['Indiranagar', 'Koramangala', 'St. Marks Road', 'Cunningham Road',
+       'Richmond Road', 'Lavelle Road', 'MG Road', 'Brigade Road',
+       'Marathahalli', 'Hennur', 'Jayanagar', 'Sarjapur',
+       'Old Airport Road', 'JP Nagar', 'Whitefield', 'Bannerghatta',
+       'Hosur Road', 'BTM', 'Kalyan Nagar', 'Ashok Nagar',
+       'Church Street', 'Basavanagudi', 'Malleshwaram', 'HSR',
+       'Brookefield', 'Ulsoor', 'Yelahanka', 'Domlur', 'Hebbal',
+       'Sahakara Nagar', 'New BEL Road', 'Nagarbhavi', 'Bhartiya City',
+       'Rajarajeshwari Nagar', 'Old Madras Road', 'Kanakapura',
+       'Bellandur', 'Electronic City', 'Race Course Road', 'Nagawara',
+       'Vasanth Nagar']
+
+user_location = st.selectbox(
+    'Which area would you like to visit today',
+    tuple(location_list))
+
+st.write('You selected:', user_location)
+
+#Get user cuisine preference
+
+cuisine_list = ['south indian', 'asian', 'thai', 'continental', 'finger food', 'north indian', 'pasta', 'pizza', 'desserts', 'beverages', 'coffee cafe',
+       'american', 'italian', 'chinese', 'biryani', 'tea', 'european',
+    'bar', 'oriental', 'bbq', 'mughlai', 'kebab',
+       'kashmiri', 'seafood', 'grilled chicken', 'fast food', 'salad',
+       'street food', 'sushi', 'lebanese', 'greek', 'moroccan',
+       'indonesian', 'andhra', 'mediterranean', 'japanese', 'burger',
+       'sandwich', 'mangalorean', 'cantonese', 'korean', 'mexican',
+       'sichuan', 'bakery', 'burmese', 'bubble tea', 'ice cream',
+       'tex-mex', 'steak', 'shake', 'healthy', 'panini', 'rolls',
+       'malaysian', 'hyderabadi', 'kerala', 'vietnamese', 'pancake',
+       'goan', 'lucknowi', 'parsi', 'turkish', 'middle eastern',
+       'gujarati', 'rajasthani', 'brazilian', 'momos', 'wraps', 'french',
+       'juices', 'awadhi', 'mithai', 'portuguese', 'arabian', 'peruvian',
+       'waffle', 'shawarma', 'mandi', 'nepalese', 'irish', 'british',
+       'bengali', 'afghan']
+
+user_cuisine = st.selectbox(
+    'Which cuisine would you like to try today',
+    tuple(cuisine_list))
+
+st.write('You selected:', user_cuisine)
+
+def input_transformer(location, cuisine):
+    location=str(location)
+    cuisine=str(cuisine)
+    location=location.replace(' ','')
+    user_input = location + ' ' + cuisine
+    final_output=('').join(user_input.lower().split('  '))
+    return final_output
+
+preprocessed_input = input_transformer(user_location, user_cuisine)
+
+#st.button("Reset", type="primary")
+if st.button('Generate Recommendations'):
+    
+    
+    
+    x_file_path = "concatenated_features.pkl"
+    # To load X from the pickle file
+    with open(x_file_path, 'rb') as file:
+        X_loaded = pickle.load(file)
+
+    # TF-IDF Vectorization
+    vector_file_path = "vectorizer.pkl"
+    # To load Vectorizer from the pickle file
+    with open(vector_file_path, 'rb') as file:
+        vectorizer = pickle.load(file)
+        
+    # TF-IDF Vectorization of user input
+    user_input_vector = vectorizer.transform([preprocessed_input])
+
+    # Calculate cosine similarity between user input and recipes
+    similarity_scores = cosine_similarity(user_input_vector, X_loaded)
+
+    # Get indices of top recommended recipes (top 3 in this example)
+    top_indices = similarity_scores.argsort()[0][-3:][::-1]
+    
+    final_df=pd.read_csv('zomato_restaurant_final.csv')
+    
+    # Display top recommended recipes
+    st.header("\nTop Recommended Restaurants:")
+    for idx in top_indices:
+        st.title(final_df.loc[idx, 'names'])
+        st.subheader("Details:")
+        st.write("Rating:", final_df.loc[idx, 'ratings'])
+        st.write("Cuisine:", final_df.loc[idx, 'cuisine_list'])
+        st.write("Location:", final_df.loc[idx, 'area_list'])
+        st.write("Price:", final_df.loc[idx, 'price for two'])
+        # st.subheader("Restaurant Name: ")
+        # st.write(final_df.loc[idx, 'names'])
+        # st.subheader("Restaurant Rating: ")
+        # st.write(final_df.loc[idx, 'ratings'])
+        # st.subheader("Cuisine: ")
+        # st.write(final_df.loc[idx, 'cuisine_list'])
+        # st.subheader("Location: ")
+        # st.write(final_df.loc[idx, 'area_list'])
+        # st.subheader("Price: ")
+        # st.write(final_df.loc[idx, 'price for two'])
+        
+    
